92efa275bc8d63a3ca80cb39cace5077312b6056
quan0512-hoang <quannlanhlung@gmail.com>
2026-01-11 18:20:44 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
+// QUAY VỀ TRANG CHÍNH
 function goHome() {
   window.location.href = "index.html";
+}
+
+// LOAD GOOGLE DOCS THEO TYPE
+const frame = document.getElementById("studyFrame");
+const params = new URLSearchParams(window.location.search);
+const type = params.get("type");
+
+// nếu iframe không tồn tại → dừng
+if (!frame) {
+  console.error("Không tìm thấy iframe");
+}
+
+// chọn nội dung
+if (type === "vocabulary") {
+  frame.src =
+    "https://docs.google.com/document/d/e/2PACX-1vTGBaXh1duKyvYFKJcN83V_IQLlo_qmMVdD5Y7wr6pDOzUXndBgTL8Ntj2QQJVJaIYTzwkEcEYFLy5K/pub?embedded=true";
+}
+else if (type === "grammar") {
+  frame.src =
+    "https://docs.google.com/document/d/e/2PACX-1vRyJLuGQmWndcXFgAXZ9r8icwKNMYmy6Kb7XBfEjRQh6tJf_YNoH4EiNp4Gm_2li5JyXEtFzhk6kwhN/pub?embedded=true";
+}
+else if (type === "writing") {
+  frame.src =
+    "https://docs.google.com/document/d/e/2PACX-1vTEKqG7SMo79PDVV_FdzAH1BUbg6AXqkvSJM5zZwpCNSsoB005pw6MqvS2fswU9JuVlyHjwyzTQtkZx/pub?embedded=true";
+}
+else {
+  // mặc định (phòng khi user vào trực tiếp study.html)
+  frame.src =
+    "https://docs.google.com/document/d/e/2PACX-1vTGBaXh1duKyvYFKJcN83V_IQLlo_qmMVdD5Y7wr6pDOzUXndBgTL8Ntj2QQJVJaIYTzwkEcEYFLy5K/pub?embedded=true";
 }
