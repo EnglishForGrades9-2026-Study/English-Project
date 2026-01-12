9bb57c1b98a4d6af7a4aa67d23d1b00e87a2b4ff
quan0512-hoang <quannlanhlung@gmail.com>
2026-01-12 20:49:21 +0700

    Update study.js

--- js/study.js
+++ js/study.js
@@ -9,10 +9,20 @@
 
 <script>
   const frame = document.getElementById("studyFrame");
-  const type = new URLSearchParams(window.location.search).get("type");
+  const params = new URLSearchParams(window.location.search);
+  const type = params.get("type");
 
   if (type === "vocabulary") {
     frame.src = "https://docs.google.com/document/d/e/2PACX-1vTGBaXh1duKyvYFKJcN83V_IQLlo_qmMVdD5Y7wr6pDOzUXndBgTL8Ntj2QQJVJaIYTzwkEcEYFLy5K/pub?embedded=true";
+  } 
+  else if (type === "grammar") {
+    frame.src = "https://docs.google.com/document/d/e/2PACX-1vRyJLuGQmWndcXFgAXZ9r8icwKNMYmy6Kb7XBfEjRQh6tJf_YNoH4EiNp4Gm_2li5JyXEtFzhk6kwhN/pub?embedded=true";
+  } 
+  else if (type === "writing") {
+    frame.src = "https://docs.google.com/document/d/e/2PACX-1vTEKqG7SMo79PDVV_FdzAH1BUbg6AXqkvSJM5zZwpCNSsoB005pw6MqvS2fswU9JuVlyHjwyzTQtkZx/pub?embedded=true";
+  } 
+  else {
+    frame.src = "https://docs.google.com";
   }
 </script>
 
