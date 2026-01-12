ffeac70407436e0b09cb4e4c2094b053ed82fecf
quan0512-hoang <quannlanhlung@gmail.com>
2026-01-12 22:47:11 +0700

    Update mindmap.js

--- js/mindmap.js
+++ js/mindmap.js
@@ -1,4 +1,7 @@
-/* ===== TOGGLE MINDMAP ===== */
-function toggle(id){
-  document.getElementById(id).classList.toggle("show");
+function showMindmap(id){
+  document.querySelectorAll('.mindmap')
+    .forEach(m => m.classList.remove('active'));
+
+  document.getElementById(id).classList.add('active');
 }
+
