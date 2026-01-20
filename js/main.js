f0c20c6e913c43af363fdc8cd1b0dde9cef64d94
quan0512-hoang <quannlanhlung@gmail.com>
2026-01-20 15:42:42 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -31,3 +31,28 @@ else {
   frame.src =
     "https://docs.google.com/document/d/e/2PACX-1vTGBaXh1duKyvYFKJcN83V_IQLlo_qmMVdD5Y7wr6pDOzUXndBgTL8Ntj2QQJVJaIYTzwkEcEYFLy5K/pub?embedded=true";
 }
+
+// nút mindmap 
+const buttons = document.querySelectorAll(".mindmap-btn");
+const modal = document.getElementById("mindmapModal");
+const frame = document.getElementById("mindmapFrame");
+const closeBtn = document.getElementById("closeModal");
+
+buttons.forEach(btn => {
+  btn.onclick = () => {
+    frame.src = btn.dataset.src;
+    modal.style.display = "flex";
+  };
+});
+
+closeBtn.onclick = () => {
+  modal.style.display = "none";
+  frame.src = "";
+};
+
+modal.onclick = (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+    frame.src = "";
+  }
+};
